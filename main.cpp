688f1da1421940b6c06200d9b25d9057cc01577a
Joao Victor Oliveira <jvoliveira3011@gmail.com>
2020-10-19 16:15:52 -0300

    Atualização do Projeto
    Totalmente funcional

--- main.cpp
+++ main.cpp
@@ -1,103 +1,133 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <fstream>
 #include <new>
-
+#include "main.hpp"
+#include <chrono>
+#include <math.h>
+#include <omp.h>
 using namespace std;
 
-// Função que converte de string para int
-int to_int(string str) {
-    stringstream ss(str);
-    int x = 0;
-    ss >> x;
-    return x;   
-}
+int main() {
 
-double *razao(double *obj, double *restricao, int tam) {
-    double *v_razao = new(nothrow) double[tam];
-
-    for (int i = 0; i < tam; i++) {
-        v_razao[i] = obj[i] / restricao[i];
-    }
-
-    return v_razao;
-}
-
-int main(){
+    cout << "*************************************************\n\n";
+    cout << "Branch-And-Bound para o Problema da Mochila 0-1\n\n";
+    cout << "*************************************************\n\n";
     ifstream entrada;
     string linha, c;
-    
+
     entrada.open("instancias/entrada01.txt"); // Abre o arquivo
 
     // Se der erro ao abrir o arquivo, envia uma mensagem de erro
-    if(!entrada.is_open()) {
+    if (!entrada.is_open()) {
         cerr << "Erro ao abrir o arquivo!";
         return false;
     }
-    
-    getline(entrada, linha); // Lê o numero de variaveis
+
+    getline(entrada, linha); // Le o numero de variaveis
     int num_variaveis = to_int(linha); //Converte o valor lido para inteiro
 
-    getline(entrada, linha); // Lê o valor do lado direito da restrição
+    getline(entrada, linha); // Le o valor do lado direito da restricao
     int b = to_int(linha);
 
-    double *restricao = new(nothrow) double[num_variaveis]; // vetor das restrições
-    double *obj = new(nothrow) double[num_variaveis]; // vetor da funcao objetivo
-    //double *razao = new(nothrow) double[num_variaveis]; // vetor da razão
-    
-    // Se der erro de alocação, retorna false
-    if(restricao == nullptr) {
-        cerr << "Erro de alocação" << endl;
-        return false; 
-    } 
-        
-    // Lê a linha da f.o e passa para um vetor
+    double* restricao = new(nothrow) double[num_variaveis]; // vetor das restricoes
+    double* obj = new(nothrow) double[num_variaveis]; // vetor da funcao objetivo
+
+    // Se der erro de alocacao, retorna false
+    if (restricao == nullptr) {
+        cerr << "Erro de alocacao" << endl;
+        return false;
+    }
+
+    // Le a linha da f.o e passa para um vetor
     getline(entrada, linha);
     int j = 0; c.clear();
-    for(int i = 0; i < linha.length(); i++){
+    for (int i = 0; i < linha.length(); i++) {
         obj[j] = 0;
-        if(linha[i] != ' ')
+        if (linha[i] != ' ')
             c += linha[i];
         else {
             obj[j] = to_int(c);
             c.clear();
-            j++;  
+            j++;
         }
-    } 
-    obj[j] = to_int(c); // Passa o ultimo valor do arquivo para a matriz
-    
-    cout << "Vetor da f.o: \n";
-    for(int i = 0; i < num_variaveis; i++){
-        cout << obj[i] << " ";
     }
-    cout << endl;
+    obj[j] = to_int(c); // Passa o ultimo valor do arquivo para a matriz
 
-    // Passa os coeficientes da restrição
+    // Passa os coeficientes da restricao
     getline(entrada, linha);
     j = 0; c.clear();
-    for(int i = 0; i < linha.length(); i++){
+    for (int i = 0; i < linha.length(); i++) {
         restricao[j] = 0;
-        if(linha[i] != ' ')
+        if (linha[i] != ' ')
             c += linha[i];
         else {
             restricao[j] = to_int(c);
             c.clear();
-            j++;  
+            j++;
         }
-    } 
+    }
     restricao[j] = to_int(c); // Passa o ultimo valor do arquivo para a matriz
 
-    cout << "Vetor das restricoes: \n";
-    for(int i = 0; i < num_variaveis; i++){
-        cout << restricao[i] << " ";
+    double* vetor_razao = razao(obj, restricao, num_variaveis);
+
+    // Inicializa a pilha
+    p.inicializar();
+
+    // Estamos usando -1 para dizer que as variaveis do nosso problema n�o tem um valor fixado
+    double* var = new(nothrow) double[num_variaveis];
+    for (int i = 0; i < num_variaveis; i++)
+        var[i] = -1;
+
+    // Empilha a relaxacao linear e adiciona aos nos abertos
+    sol relaxacao_linear = relaxacao(obj, restricao, num_variaveis, b, var);
+    p.empilhar(relaxacao_linear);
+    relatoriof.nos_abertos++;	
+
+    cout << "Numero de variaveis do problema: " << num_variaveis << "\n\n";
+
+    auto start = chrono::steady_clock::now();
+    
+    #pragma omp parallel
+    {
+        BranchBound(relaxacao_linear, obj, restricao, num_variaveis, b, var);
     }
-    cout << endl;
 
-    double *vetor_razao = razao(obj, restricao, num_variaveis);
+    auto end = chrono::steady_clock::now();
 
-    cout << "Razao de cada variavel: " << endl;
-    for(int i = 0; i < num_variaveis; i++) {
-        cout << vetor_razao[i] << " ";
-    }    
+    p.terminar();
+
+    // Se houver solu��o inteira, mostra os resultados, se nao, avisa que n�o h� solu��o inteira
+    if (solint.z != 0) {
+
+        cout << "Numero de nos avaliados: " << relatoriof.nos_avaliados << "\n";
+
+        cout << "Tempo decorrido: "
+            << chrono::duration_cast<chrono::nanoseconds>(end - start).count() / 1000000000.0 << " segundos\n";
+
+        cout << "Gap: " << relatoriof.gap << "%\n";
+        printar_solucao(solint, num_variaveis);
+    }
+    else {
+        cout << "Numero de nos avaliados: " << relatoriof.nos_avaliados << "\n";
+
+        cout << "Tempo decorrido: "
+            << chrono::duration_cast<chrono::nanoseconds>(end - start).count() / 1000000000.0 << " segundos\n";
+		
+		if (relatoriof.nos_avaliados == MaxNos){
+            cout << "Programa encerrado devido ao limite de nos.\n";
+			cout << "\nNao foi possivel encontrar uma solucao inteira.\n";
+        }
+		else
+			cout << "\nNao foi possivel encontrar uma solucao inteira.\n";
+    }
+
+    delete[] restricao;
+    delete[] vetor_razao;
+    delete[] obj;
+    delete[] relaxacao_linear.var;
+    delete[] solint.var;
 }
